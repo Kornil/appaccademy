814110426332319973351d6801d3b04f6656891c
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2020-01-10 14:07:17 +0100

    Finish bootcamp_project

---
+++ bootcamp_project/lib/bootcamp.rb
@@ -0,0 +1,52 @@
+class Bootcamp
+  def initialize(name, slogan, student_capacity)
+    @name = name
+    @slogan = slogan
+    @student_capacity = student_capacity
+    @teachers = []
+    @students = []
+    @grades = Hash.new { |h, k| h[k] = [] }
+  end
+
+  attr_reader :name, :slogan, :teachers, :students
+
+  def hire(teacher)
+    @teachers << teacher
+  end
+
+  def enroll(student)
+    if @students.length < @student_capacity
+      @students << student
+      true
+    else
+      false
+    end
+  end
+
+  def enrolled?(student)
+    @students.include?(student)
+  end
+
+  def student_to_teacher_ratio
+    @students.length / @teachers.length
+  end
+
+  def add_grade(student, grade)
+    if @students.include?(student)
+      @grades[student] << grade
+      true
+    else
+      false
+    end
+  end
+
+  def num_grades(student)
+    @grades[student].length
+  end
+
+  def average_grade(student)
+    if @students.include?(student) & @grades[student].length.positive?
+      @grades[student].reduce(:+) / @grades[student].length
+    end
+  end
+end
